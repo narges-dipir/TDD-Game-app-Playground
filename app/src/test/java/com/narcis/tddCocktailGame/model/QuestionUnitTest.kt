4bb55381e5ab03f5fb7069b294f70018a8d82278
narges-dpr <nargesdeypir@gmail.com>
2023-12-15 17:31:12 +0330

    game class -- checking given correct answer - red

--- app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
+++ app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
@@ -2,20 +2,32 @@ package com.narcis.tddCocktailGame.model
 
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
+
+private const val INCORRECT = "INCORRECT"
+
+private const val CORRECT = "CORRECT"
 
 class QuestionUnitTest {
 
     @Test
     fun `when creating question should not have answered option`() {
-        val question = Question("CORRECT", "INCORRECT")
+        val question = Question(CORRECT, INCORRECT)
         assertNull(question.answeredOption)
     }
 
     @Test
     fun `when answering should have answered option`() {
-        val question = Question("CORRECT", "INCORRECT")
-        question.answer("INCORRECT")
-        assertEquals("INCORRECT", question.answeredOption)
+        val question = Question(CORRECT, INCORRECT)
+        question.answer(INCORRECT)
+        assertEquals(INCORRECT, question.answeredOption)
+    }
+
+    @Test
+    fun `when answering with correct option should return true`() {
+        val question = Question(CORRECT, INCORRECT)
+        val result = question.answer(CORRECT)
+        assertTrue(result)
     }
 }
