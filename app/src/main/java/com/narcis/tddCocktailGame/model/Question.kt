d14daf07ebf9527dcde8bc1e400d9aafd7df4f83
narges-dpr <nargesdeypir@gmail.com>
2023-12-15 18:07:28 +0330

    game class -- checking given exception for illegal answer - green

--- app/src/main/java/com/narcis/tddCocktailGame/model/Question.kt
+++ app/src/main/java/com/narcis/tddCocktailGame/model/Question.kt
@@ -8,6 +8,9 @@ class Question(
         private set
 
     fun answer(option: String): Boolean {
+        if (option != correctOption && option != incorrectOption) {
+            throw IllegalArgumentException("Not a valid option")
+        }
         answeredOption = option
 
         return correctOption == answeredOption
