df5ec784def0d9f01fbf5f35a791f3382f33eb1c
narges-dpr <nargesdeypir@gmail.com>
2023-12-15 17:19:31 +0330

    game class -- saving answer options - red

--- app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
+++ app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
@@ -1,5 +1,6 @@
 package com.narcis.tddCocktailGame.model
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 
@@ -9,5 +10,12 @@ class QuestionUnitTest {
     fun `when creating question should not have answered option`() {
         val question = Question("CORRECT", "INCORRECT")
         assertNull(question.answeredOption)
+    }
+
+    @Test
+    fun `when answering should have answered option`() {
+        val question = Question("CORRECT", "INCORRECT")
+        question.answer("INCORRECT")
+        assertEquals("INCORRECT", question.answeredOption)
     }
 }
