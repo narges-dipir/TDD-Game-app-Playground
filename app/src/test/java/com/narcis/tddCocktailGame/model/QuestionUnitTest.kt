783a50f65cc63b473ccfe6e869398770e4c063ae
narges-dpr <nargesdeypir@gmail.com>
2023-12-15 17:57:20 +0330

    game class -- checking given exception for illegal answer - red

--- app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
+++ app/src/test/java/com/narcis/tddCocktailGame/model/QuestionUnitTest.kt
@@ -5,10 +5,13 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.lang.IllegalArgumentException
 
 private const val INCORRECT = "INCORRECT"
 
 private const val CORRECT = "CORRECT"
+
+private const val INVALID = "INVALID"
 
 class QuestionUnitTest {
 
@@ -37,5 +40,12 @@ class QuestionUnitTest {
         val question = Question(CORRECT, INCORRECT)
         val result = question.answer(INCORRECT)
         assertFalse(result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `when answering with invalid option should throw exception`() {
+        val question = Question(CORRECT, INCORRECT)
+
+        question.answer(INVALID)
     }
 }
