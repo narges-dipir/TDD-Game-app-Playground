26030a057de70a294484e6bbcf6ea757074534af
narges-dpr <nargesdeypir@gmail.com>
2023-12-20 10:06:40 +0330

    mockito- cocktailsRepository save score via shared preferences -- green

--- app/src/main/java/com/narcis/tddCocktailGame/common/repository/CocktailsRepositoryImpl.kt
+++ app/src/main/java/com/narcis/tddCocktailGame/common/repository/CocktailsRepositoryImpl.kt
@@ -3,11 +3,14 @@ package com.narcis.tddCocktailGame.common.repository
 import android.content.SharedPreferences
 import com.narcis.tddCocktailGame.common.network.CocktailsApi
 
+private const val HIGH_SCORE_KEY = "HIGH_SCORE_KEY"
 class CocktailsRepositoryImpl(
     private val api: CocktailsApi,
     private val sharedPreferences: SharedPreferences,
 ) : CocktailsRepository {
     override fun saveHighScore(score: Int) {
-        TODO("Not yet implemented")
+        val editor = sharedPreferences.edit()
+        editor.putInt(HIGH_SCORE_KEY, score)
+        editor.apply()
     }
 }
