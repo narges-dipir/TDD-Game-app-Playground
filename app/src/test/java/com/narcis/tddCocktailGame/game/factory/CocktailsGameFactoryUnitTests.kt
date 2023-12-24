f928fe816ceac295ec12a3d2cb5dedb1826af27d
n.dipir <nargesdeypir@gmail.com>
2023-12-24 10:32:38 +0330

    mockito- game factory - calling onSuccess -- red

--- app/src/test/java/com/narcis/tddCocktailGame/game/factory/CocktailsGameFactoryUnitTests.kt
+++ app/src/test/java/com/narcis/tddCocktailGame/game/factory/CocktailsGameFactoryUnitTests.kt
@@ -1,15 +1,26 @@
 package com.narcis.tddCocktailGame.game.factory
 
+import com.narcis.tddCocktailGame.common.network.Cocktail
 import com.narcis.tddCocktailGame.common.repository.CocktailsRepository
+import com.narcis.tddCocktailGame.common.repository.RepositoryCallback
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class CocktailsGameFactoryUnitTests {
     private lateinit var repository: CocktailsRepository
     private lateinit var factory: CocktailsGameFactory
+
+    private val cocktails = listOf(
+        Cocktail("1", "Drink1", "image1"),
+        Cocktail("2", "Drink2", "image2"),
+        Cocktail("3", "Drink3", "image3"),
+        Cocktail("4", "Drink4", "image4"),
+    )
 
     @Before
     fun setUp() {
@@ -21,5 +32,22 @@ class CocktailsGameFactoryUnitTests {
     fun `build game should get cocktails from repo`() {
         factory.buildGame(mock())
         verify(repository).getAlcoholic(any())
+    }
+
+    @Test
+    fun `buildGame should call onSuccess`() {
+        val callback = mock<CocktailsGameFactory.Callback>()
+        setUpRepositoryWithCocktails(repository)
+        factory.buildGame(callback)
+        verify(callback).onSuccess(any())
+    }
+
+    private fun setUpRepositoryWithCocktails(
+        repository: CocktailsRepository,
+    ) {
+        doAnswer {
+            val callback: RepositoryCallback<List<Cocktail>, String> = it.getArgument(0)
+            callback.onSuccess(cocktails)
+        }.whenever(repository).getAlcoholic(any())
     }
 }
