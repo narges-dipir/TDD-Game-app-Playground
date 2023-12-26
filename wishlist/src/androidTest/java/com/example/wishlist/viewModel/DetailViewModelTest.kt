2e03f4b9e71c9672add8fa83070c548390c35041
n.dipir <nargesdeypir@gmail.com>
2023-12-26 11:06:59 +0330

    wishlist app tdd -- DetailViewModelTest - should save a correct new item -- red

--- wishlist/src/androidTest/java/com/example/wishlist/viewModel/DetailViewModelTest.kt
+++ wishlist/src/androidTest/java/com/example/wishlist/viewModel/DetailViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.example.wishlist.viewModel
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.wishlist.dataPersistance.RepositoryImpl
 import com.example.wishlist.dataPersistance.WishlistDao
@@ -9,8 +10,10 @@ import com.example.wishlist.model.Wishlist
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.component.getScopeId
 import org.mockito.Mockito
 import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
@@ -26,5 +29,16 @@ class DetailViewModelTest {
         val wishItem = Wishlist("Victoria", listOf("RW Android Apprentice book", "AndroidPhone"), 1)
         viewModel.saveNewItem(wishItem, "smart watch")
         verify(wishlistDao).save(any())
+    }
+
+    @Test
+    fun saveNewItemSavesData() {
+        val wishItem = Wishlist("Victoria", listOf("RW Android Apprentice book", "AndroidPhone"), 1)
+        val name = "smart watch"
+        viewModel.saveNewItem(wishItem, name)
+        val mockObserver = mock<Observer<Wishlist>>()
+
+        wishlistDao.findById(wishItem.id).observeForever(mockObserver)
+        verify(mockObserver).onChanged(wishItem.copy(wishes = wishItem.wishes + name))
     }
 }
