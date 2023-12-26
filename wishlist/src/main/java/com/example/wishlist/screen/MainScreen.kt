0799f34f0769d885befd097319616d1e93044c3f
n.dipir <nargesdeypir@gmail.com>
2023-12-26 09:45:12 +0330

    wishlist app tdd -- initial project - basic views -- main screen; WIP

--- wishlist/src/main/java/com/example/wishlist/screen/MainScreen.kt
+++ wishlist/src/main/java/com/example/wishlist/screen/MainScreen.kt
@@ -1,6 +1,22 @@
 package com.example.wishlist.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.wishlist.viewModel.MainViewModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -8,4 +24,29 @@ import org.koin.androidx.compose.koinViewModel
 fun MainScreen(
     viewModel: MainViewModel = koinViewModel(),
 ) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.fillMaxHeight(0.9f))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth(0.8f).padding(12.dp),
+        ) {
+            Text(
+                text = "ADD LIST",
+                color = Color.White,
+                modifier = Modifier.background(color = Color.White.copy(alpha = 0.1f)),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MainScreen()
 }
