8f10affe3b6a2c475d677d77494e718349ebdca7
n.dipir <nargesdeypir@gmail.com>
2023-12-27 09:48:46 +0330

    wishlist app -- data persistence : test getAll() for real (testing query) -- green

--- wishlist/src/main/java/com/example/wishlist/dataPersistance/StringListConverter.kt
+++ wishlist/src/main/java/com/example/wishlist/dataPersistance/StringListConverter.kt
@@ -12,5 +12,9 @@ object StringListConverter {
     @TypeConverter
     @JvmStatic
     fun stringToStringList(string: String?): MutableList<String>? =
-        string?.split("|")?.toMutableList()
+        if (!string.isNullOrBlank()) {
+            string.split("|").toMutableList()
+        } else {
+            mutableListOf()
+        }
 }
