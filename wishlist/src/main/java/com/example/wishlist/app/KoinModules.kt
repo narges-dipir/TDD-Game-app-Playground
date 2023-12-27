1a3c05d8f5e7c21badbc04334f4e9465fed3bb38
n.dipir <nargesdeypir@gmail.com>
2023-12-27 10:11:03 +0330

    wishlist app -- data persistence : building db

--- wishlist/src/main/java/com/example/wishlist/app/KoinModules.kt
+++ wishlist/src/main/java/com/example/wishlist/app/KoinModules.kt
@@ -1,9 +1,9 @@
 package com.example.wishlist.app
 
+import androidx.room.Room
 import com.example.wishlist.dataPersistance.Repository
 import com.example.wishlist.dataPersistance.RepositoryImpl
-import com.example.wishlist.dataPersistance.WishlistDao
-import com.example.wishlist.dataPersistance.WishlistDaoImpl
+import com.example.wishlist.dataPersistance.WishlistDatabase
 import com.example.wishlist.viewModel.DetailViewModel
 import com.example.wishlist.viewModel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -13,7 +13,16 @@ val appModule = module {
 
     single<Repository> { RepositoryImpl(get()) }
 
-    single<WishlistDao> { WishlistDaoImpl() }
+    single {
+        Room.databaseBuilder(
+            get(),
+            WishlistDatabase::class.java,
+            "wishlist-database",
+        )
+            .allowMainThreadQueries()
+            .build()
+            .wishListDao()
+    }
 
     viewModel { MainViewModel(get()) }
 
