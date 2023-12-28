01cc83f83ef1a735b42a7b80fc1336552c514630
narges-dpr <nargesdeypir@gmail.com>
2023-12-28 22:50:25 +0330

    punchline game -- assert getRandomJoke() emit value with 200 response -- red

--- punchline/src/test/java/com/narcis/punchline/network/JokeServiceTest.kt
+++ punchline/src/test/java/com/narcis/punchline/network/JokeServiceTest.kt
@@ -1,13 +1,17 @@
 package com.narcis.punchline.network
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.narcis.punchline.Joke
 import com.narcis.punchline.JokeService
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Rule
+import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class JokeServiceTestUsingWebServer {
+    private val testJson = """{"id":1, "joke": "joke"}"""
     @get:Rule
     val mockWebServer = MockWebServer()
 
@@ -20,5 +24,14 @@ class JokeServiceTestUsingWebServer {
     }
     private val jokeService by lazy {
         retrofit.create(JokeService::class.java)
+    }
+
+    @Test
+    fun getRandomJokeEmitsJoke() {
+        mockWebServer.enqueue(
+            MockResponse().setBody(testJson).setResponseCode(200)
+        )
+        val testObserver = jokeService.getRandomeJoke().test()
+        testObserver.assertValue(Joke("1", "joke"))
     }
 }
